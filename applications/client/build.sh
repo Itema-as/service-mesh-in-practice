4d434c69d08eecd1153c73f90cbbf9490ac1b61a
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2021-07-19 22:54:19 +0200

    rename folder for client

---
+++ applications/client/build.sh
@@ -0,0 +1,5 @@
+# Build image
+docker build app/. -t jafossum/itemacon-client
+
+# Push image
+docker push jafossum/itemacon-client
--- applications/simple-client/build.sh
+++
@@ -1,5 +0,0 @@
-# Build image
-docker build app/. -t jafossum/itemacon-simple-client
-
-# Push image
-docker push jafossum/itemacon-simple-client
