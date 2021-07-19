cc7a2be8808b01f023f5fd3bb37d812d6e11e3c9
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2021-07-19 22:55:20 +0200

    04-script corrections

--- service-mesh/04-service-account/apply.sh
+++ service-mesh/04-service-account/apply.sh
@@ -6,5 +6,6 @@ kubectl apply -f ../resources/vs-retry-failure.yaml
 kubectl apply -f ../resources/auth-policy-sa.yaml
 
 read -n 1 -s -r -p "Press any key to continue"
+echo ""
 
 kubectl apply -f ../resources/client-sa.yaml
--- service-mesh/04-service-account/delete.sh
+++ service-mesh/04-service-account/delete.sh
@@ -1,7 +1,7 @@
-kubectl apply -f ../resources/server.yaml
-kubectl apply -f ../resources/client.yaml
+kubectl delete -f ../resources/server.yaml
+kubectl delete -f ../resources/client.yaml
 
-kubectl apply -f ../resources/vs-retry-failure.yaml
+kubectl delete -f ../resources/vs-retry-failure.yaml
 
-kubectl apply -f ../resources/auth-policy-sa.yaml
-kubectl apply -f ../resources/client-sa.yaml
+kubectl delete -f ../resources/auth-policy-sa.yaml
+kubectl delete -f ../resources/client-sa.yaml
