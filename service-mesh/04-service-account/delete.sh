d83ca4d68b03786d227d20c61a86df9fd2c56e12
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2021-09-02 22:32:18 +0200

    add service-account to exercise

--- service-mesh/04-service-account/delete.sh
+++ service-mesh/04-service-account/delete.sh
@@ -5,6 +5,7 @@ kubectl delete -f ../resources/client.yaml
 
 kubectl delete -f ../resources/vs-retry-failure.yaml
 
+kubectl delete -f ../resources/service-account.yaml
 kubectl delete -f ../resources/auth-deny-all.yaml
 kubectl delete -f ../resources/auth-policy-sa.yaml
 kubectl delete -f ../resources/client-sa.yaml
