840092bb0325365d2c0dd392ca0c5ad6de125819
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2021-09-02 22:34:45 +0200

    upgrade versions

--- service-mesh/install-istio.sh
+++ service-mesh/install-istio.sh
@@ -2,11 +2,11 @@
 
 # Install istio onto Kubernetes cluster
 
-VERSION_ISTO=1.10.3
+VERSION_ISTO=1.11.1
 
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$VERSION_ISTO sh -
 mv istio-$VERSION_ISTO istio
 cd istio || exit
-export PATH=$PWD/bin:$PATH
-istioctl install --set profile=demo -y
+# export PATH=$PWD/bin:$PATH
+./bin/istioctl install --set profile=demo -y
 kubectl label namespace default istio-injection=enabled
